f7bbfc469539c23ed40e7e94d865a5e6389e248c
Peach <scdzwyxst@gmail.com>
2014-12-04 16:51:28 +0800

    Add shortcut: press backspace to focus the search box and rewrite keywords, and focus the search box when the page is ready

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,32 @@ angular.module('abstackVarLib', ['zeroclipboard'])
           swfPath: '../bower_components/zeroclipboard/dist/ZeroClipboard.swf'
         });
     }])
+    .directive('readyFocus', ['$document', function($document){
+        return {
+            restrict: 'A',
+            link: function($scope, iElm, iAttrs, controller) {
+                $document.ready(function(){
+                   iElm[0].focus(); 
+                });
+            }
+        };
+    }])
+    .directive('backspaceFocus', function(){
+        return {
+            restrict: 'A',
+            link: function($scope, iElm, iAttrs, controller) {
+                var targetObj = document.getElementById(iAttrs['backspaceFocus']);
+
+                iElm[0].onkeydown = function(ev){
+                    var event = ev || event,
+                        keyCode = event.keyCode;
+                    if(keyCode == 8 && targetObj){
+                        targetObj.focus();
+                    }
+                };
+            }
+        };
+    })
     .filter('keywords', function(){
         return function(input, type){
             if(!type)
