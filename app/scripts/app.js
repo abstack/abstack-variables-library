4dac231bc5d73148caa718371cef14c893c63cce
Peach <scdzwyxst@gmail.com>
2014-12-04 14:37:08 +0800

    Create a filter to filter words

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,20 @@
 * Description: main
 */
 angular.module('abstackVarLib', [])
+    .filter('keywords', function(){
+        return function(input, type){
+            if(!type)
+                return input;
+
+            var temp = [];
+            for(var i = 0;i<input.length;i++){
+                if(input[i].tags.join().indexOf(type) != -1)
+                    temp.push(input[i]);
+            }
+
+            return temp;
+        }
+    })
     .controller('mainCtrl', ['$http', '$scope', function($http, $scope){
 
         $http.get('/json/words.json')
